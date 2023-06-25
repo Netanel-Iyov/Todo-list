c9adb490df4d60dde2cec915ef4abc24a43657b9
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2023-06-25 12:07:53 +0300

    configure favicon

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -53,7 +53,6 @@ module.exports = {
     // fix "process is not defined" error:
       new webpack.ProvidePlugin({
         process: 'process/browser',
-        favicon: "./public/favicon.ico"
       }),
   ]
 };
