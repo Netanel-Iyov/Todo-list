9bc3d30440dabbc64c79cb040a335aa44b338cd4
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2023-07-01 01:49:46 +0300

    adding description and collapse option to GUI

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,7 @@
-import { useState, useEffect, useRef , Fragment } from "react";
+import { useState, useEffect , Fragment } from "react";
 import { useNavigate } from 'react-router-dom';
 import { checkIfConnected, API_BASE } from '../utils/utils'
-import { PlusCircleIcon, ArrowLeftCircleIcon, XCircleIcon, TrashIcon, PencilSquareIcon } from '@heroicons/react/20/solid'
+import { PlusCircleIcon, ArrowLeftCircleIcon, XCircleIcon, TrashIcon, PencilSquareIcon, ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/20/solid'
 
 
 const HomePage = () => {
@@ -10,14 +10,16 @@ const HomePage = () => {
 
     const [todos, setTodos] = useState([]);
     
+    const [todoTitle, setTodoTitle] = useState("")
+    const [todoDescription, setTodoDescription] = useState("")
+
     const [newPopupActive, setNewPopupActive] = useState(false)
-    const [newTodo, setNewTodo] = useState("")
-
     const [editPopUpActive, setEditPopupActive] = useState(false)
-    const [todoToEdit, setTodoToEdit] = useState(undefined)
-    const [todoToEditText, setTodoToEditText] = useState("")
-    
 
+    const [todoToEdit, setTodoToEdit] = useState(undefined)
+
+    const [toggleCollapse, setToggleCollapse] = useState([])
+    
 
     useEffect (() => {
         handleConnection();
@@ -47,7 +49,9 @@ const HomePage = () => {
             if (response.ok) {
                 const data = await response.json()
                 setTodos(data)
+                setToggleCollapse(Array.from({ length: data.length }).fill(false))
             }
+
         } catch(error) {
             console.error("While getting Todos the following error occured : ", error)
         }
@@ -75,13 +79,13 @@ const HomePage = () => {
             method: 'DELETE'
         }
         const deletedTodo = await fetch(API_BASE + '/todo/delete/' + id, requestOptions)
-        .then(res => res.json()).then(data => console.log('"' + data.text + '" Was deleted successfully'))
+        .then(res => res.json()).then(data => console.log('"' + data.title + '" Was deleted successfully'))
 
         GetTodos()
     }
 
-    const addTodo = async (text) => {
-        if (text === '') {
+    const addTodo = async (title, description) => {
+        if (title === '') {
             return
         }
         const requestOptions = {
@@ -89,7 +93,7 @@ const HomePage = () => {
             headers: {
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify({token: localStorage.token, "text": text}) 
+            body: JSON.stringify({token: localStorage.token, "title": title, "description": description}) 
         }
         const response = await fetch(API_BASE + '/todo/new/', requestOptions)
         .then(res => res.json())
@@ -97,14 +101,15 @@ const HomePage = () => {
 
         setTodos([...todos, response])
         setNewPopupActive(false)
-        setNewTodo("")
+        setTodoTitle("")
+        setTodoDescription("")
     }
 
     const editTodo = async(TodoObject) => {
         const requestOptions = {
             method: 'PUT',
             headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({text: todoToEditText})
+            body: JSON.stringify({title: todoTitle, description: todoDescription})
         }
         await fetch(API_BASE + `/todo/update/${TodoObject._id}`, requestOptions)
             .then(res => res.json())
@@ -113,7 +118,8 @@ const HomePage = () => {
 
         setEditPopupActive(false)
         setTodoToEdit(undefined)
-        setTodoToEditText("")
+        setTodoTitle("")
+        setTodoDescription("")
     }
 
     const signOut = () => {
@@ -121,26 +127,36 @@ const HomePage = () => {
         navigate('/login')
     }
 
-    const createPopupFragment = (title, inputValue, setInputValue, buttonText, buttonAction, exitOnClick) => {
+    const createPopupFragment = (popUpHeader, buttonText, buttonAction, exitOnClick) => {
         return (
           <Fragment>
-            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[400px] bg-[#EEE] p-8 rounded-lg shadow-lg">
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[400px] bg-[#EEE] p-2 pb-8 rounded-lg shadow-lg">
               <XCircleIcon
-                className="fill-[#D81E5B] absolute top-6 right-3 cursor-pointer transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center"
+                className="fill-[#D81E5B] absolute top-4 right-2 cursor-pointer transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center"
                 onClick={() => exitOnClick(false)}
               />
               <div>
-                <h3 className="text-gray-800 mb-4 font-bold">{title}</h3>
+                <h3 className="text-gray-800 mb-1 mt-1 font-bold text-lg flex justify-center">{popUpHeader}</h3>
+                <label className="text-gray-800 text-base p-0">Title</label>
                 <input
-                  id="popUpTextInput"
+                  id="popUpTitleInput"
                   type="text"
-                  className="translate-y-2 text-gray-800 appearance-none h-10 min-w-max border-0 focus:border-purple-600 outline-none bg-white p-4 rounded-2xl shadow-md w-full text-base"
-                  onChange={(e) => setInputValue(e.target.value)}
-                  value={inputValue}
+                  className="translate-y-2 text-gray-800 h-8 min-w-max border-0 focus:border-purple-800 outline-none bg-white rounded-md shadow-md w-full text-base mb-4"
+                  onChange={(e) => setTodoTitle(e.target.value)}
+                  value={todoTitle}
+                />
+
+                <label className="text-gray-800">Description</label>
+                <textarea 
+                  id="popUpDescriptionInput"
+                  rows="10"
+                  className="translate-y-2 text-gray-800 appearance-none h-36 min-w-max border-0 focus:border-purple-800 outline-none bg-white rounded-md shadow-md w-full text-base"
+                  onChange={(e) => setTodoDescription(e.target.value)}
+                  value={todoDescription}
                 />
       
                 <button
-                  className="translate-y-4 mt-4 h-9 w-40 translate-x-20 transform cursor-pointer rounded-3xl bg-gradient-to-r from-[#D81E5B] to-[#8A4EFC] text-center text-lg font-semibold"
+                  className="translate-y-4 mt-4 h-9 w-40 translate-x-28 transform cursor-pointer rounded-3xl bg-gradient-to-r from-[#D81E5B] to-[#8A4EFC] text-center text-lg font-semibold"
                   onClick={(event) => {
                     event.stopPropagation();
                     buttonAction();
@@ -153,11 +169,11 @@ const HomePage = () => {
       };
 
     const createNewPopUpFragment = () => {
-    return createPopupFragment('Add Task', newTodo, setNewTodo, 'Create Task', () => addTodo(newTodo), setNewPopupActive)
+    return createPopupFragment('Add Task', 'Create Task', () => addTodo(todoTitle, todoDescription), setNewPopupActive)
     }
       
     const createEditPopUpFragment = () => {
-    return createPopupFragment('Edit Task', todoToEditText, (value) => setTodoToEditText(value), 'Update', () => editTodo(todoToEdit), setEditPopupActive)
+    return createPopupFragment('Edit Task', 'Update Task', () => editTodo(todoToEdit), setEditPopupActive)
     }
 
     return (
@@ -165,39 +181,57 @@ const HomePage = () => {
             <h1 className="text-4xl font-bold mb-8">Welcome, {`${localStorage.first_name}`} </h1>
             
             <div className="flex justify-between">
-                <h4 className="order-1 text-base text-[#61759b] uppercase font-normal mb-4">Your tasks</h4>
+                <h4 className="order-1 text-base text-[#61759b] uppercase font-normal">Your tasks</h4>
             </div>
-            
-            
-            <div>
-                { todos.map(todo => ( 
-                    <div className="relative bg-[#131A26] p-4 rounded-2xl flex items-center transition duration-500 cursor-pointer mb-4 hover:opacity-80" key={todo._id} onClick={() => completeTodo(todo._id)}>
-                        <div className={"w-5 h-5 mr-4 rounded-full bg-[#61759b] transition duration-400 " + (todo.complete ? " bg-[#D81E5B] bg-gradient-to-b from-[#D81E5B] to-[#8A4EFC]":"")}></div>
-                        <div className={"text-base" + (todo.complete ? " line-through" : "")}>{todo.text}</div>
-                            <TrashIcon className="fill-[#D81E5B] absolute top-1/2 right-4 transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center" 
-                                onClick={(event) => {
-                                    event.stopPropagation()
-                                    deleteTodo(todo._id)
-                                }
-                            }/>
-                            <PencilSquareIcon className="absolute top-1/2 right-12 transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center" 
-                                onClick={(event) => {
-                                    event.stopPropagation()
-                                    setEditPopupActive(true)
-                                    setTodoToEdit(todo)
-                                    setTodoToEditText(todo.text)
-                                }
-                            }/>
 
+            <div>
+                { todos.map((todo, index) => ( 
+                    <div key={todo._id}>
+                        <div className="relative bg-[#131A26] p-4 rounded-2xl flex items-center transition duration-500 cursor-pointer mt-4 hover:opacity-80" onClick={() => completeTodo(todo._id)}>
+                            <div className={"w-5 h-5 mr-4 rounded-full bg-[#61759b] transition duration-400 " + (todo.complete ? " bg-[#D81E5B] bg-gradient-to-b from-[#D81E5B] to-[#8A4EFC]":"")}></div>
+                            <div className={"text-base" + (todo.complete ? " line-through" : "")}>{todo.title}</div>
+                                <TrashIcon className="fill-[#D81E5B] absolute top-1/2 right-4 transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center" 
+                                    onClick={(event) => {
+                                        event.stopPropagation()
+                                        deleteTodo(todo._id)
+                                    }
+                                }/>
+                                <PencilSquareIcon className="absolute top-1/2 right-12 transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center" 
+                                    onClick={(event) => {
+                                        event.stopPropagation()
+                                        setEditPopupActive(true)
+                                        setTodoToEdit(todo)
+                                        setTodoTitle(todo.title)
+                                        setTodoDescription(todo.description)
+                                    }
+                                }/>
+                                
+                                <ChevronDownIcon className="absolute top-1/2 right-20 transform -translate-y-1/2 w-8 h-8 rounded-full flex items-center justify-center" 
+                                    onClick={(event) => {
+                                        event.stopPropagation()
+                                        const arrayToUpdate = [...toggleCollapse]
+                                        arrayToUpdate[index] = !arrayToUpdate[index]
+                                        setToggleCollapse(arrayToUpdate)
+                                        console.log(toggleCollapse)
+                                    }
+                                }/>
+
+                        </div>
+
+                        {toggleCollapse[index] ? 
+                            <div className="relative bg-[#131A26] p-4 rounded-2xl transition duration-500 mb-4 mt-1 hover:opacity-80">
+                                <p className="mb-2 text-gray-400 dark:text-gray-400">{todo.description}</p>
+                                {/* <p className="text-gray-500 dark:text-gray-400">Check out this guide to learn how to <a href="/docs/getting-started/introduction/" className="text-blue-600 dark:text-blue-500 hover:underline">get started</a> and start developing websites even faster with components on top of Tailwind CSS.</p> */}
+                            </div> : ""
+                        }
                     </div>
                 )) } 
             </div>
             
-            <div className="fixed bottom-8 left-8 flex items-center justify-center w-32 h-12 rounded-full text-base font-bold text-gray-200 bg-gradient-to-br from-[#D81E5B] to-[#8A4EFC] cursor-pointer" onClick={() => signOut(true)}>Sign Out&nbsp;
+            <div className="fixed bottom-8 left-8 flex items-center justify-center w-32 h-12 rounded-full text-base font-bold bg-gray-200 bg-gradient-to-br from-[#D81E5B] to-[#8A4EFC] cursor-pointer" onClick={() => signOut(true)}>Sign Out&nbsp;
                 <ArrowLeftCircleIcon className="h-6 w-6"/> 
             </div>
             
-
             <div className="fixed bottom-8 right-8 flex items-center justify-center w-32 h-12 rounded-full text-base font-bold text-gray-200 bg-gradient-to-br from-[#D81E5B] to-[#8A4EFC] cursor-pointer" onClick={() => setNewPopupActive(true)}>
                 Add Task&nbsp;
                 <PlusCircleIcon className="h-6 w-6"/>
