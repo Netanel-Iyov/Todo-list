4ba7eb2c04a9f066d981d6d571195be8f4fe900e
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2023-06-30 11:23:43 +0300

    adding editing option for todos

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useLayoutEffect, Fragment } from "react";
+import { useState, useEffect, useRef , Fragment } from "react";
 import { useNavigate } from 'react-router-dom';
 import { checkIfConnected, API_BASE } from '../utils/utils'
 import { PlusCircleIcon, ArrowLeftCircleIcon, XCircleIcon, TrashIcon, PencilSquareIcon } from '@heroicons/react/20/solid'
@@ -9,8 +9,14 @@ const HomePage = () => {
     const navigate = useNavigate();
 
     const [todos, setTodos] = useState([]);
-    const [popupActive, setPopupActive] = useState(false)
+    
+    const [newPopupActive, setNewPopupActive] = useState(false)
     const [newTodo, setNewTodo] = useState("")
+
+    const [editPopUpActive, setEditPopupActive] = useState(false)
+    const [todoToEdit, setTodoToEdit] = useState(undefined)
+    const [todoToEditText, setTodoToEditText] = useState("")
+    
 
 
     useEffect (() => {
@@ -90,8 +96,24 @@ const HomePage = () => {
         .catch('Error adding data has occured')
 
         setTodos([...todos, response])
-        setPopupActive(false)
+        setNewPopupActive(false)
         setNewTodo("")
+    }
+
+    const editTodo = async(TodoObject) => {
+        const requestOptions = {
+            method: 'PUT',
+            headers: {'Content-type': 'application/json'},
+            body: JSON.stringify({text: todoToEditText})
+        }
+        await fetch(API_BASE + `/todo/update/${TodoObject._id}`, requestOptions)
+            .then(res => res.json())
+
+        GetTodos()
+
+        setEditPopupActive(false)
+        setTodoToEdit(undefined)
+        setTodoToEditText("")
     }
 
     const signOut = () => {
@@ -99,6 +121,44 @@ const HomePage = () => {
         navigate('/login')
     }
 
+    const createPopupFragment = (title, inputValue, setInputValue, buttonText, buttonAction, exitOnClick) => {
+        return (
+          <Fragment>
+            <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[400px] bg-[#EEE] p-8 rounded-lg shadow-lg">
+              <XCircleIcon
+                className="fill-[#D81E5B] absolute top-6 right-3 cursor-pointer transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center"
+                onClick={() => exitOnClick(false)}
+              />
+              <div>
+                <h3 className="text-gray-800 mb-4 font-bold">{title}</h3>
+                <input
+                  id="popUpTextInput"
+                  type="text"
+                  className="translate-y-2 text-gray-800 appearance-none h-10 min-w-max border-0 focus:border-purple-600 outline-none bg-white p-4 rounded-2xl shadow-md w-full text-base"
+                  onChange={(e) => setInputValue(e.target.value)}
+                  value={inputValue}
+                />
+      
+                <button
+                  className="translate-y-4 mt-4 h-9 w-40 translate-x-20 transform cursor-pointer rounded-3xl bg-gradient-to-r from-[#D81E5B] to-[#8A4EFC] text-center text-lg font-semibold"
+                  onClick={(event) => {
+                    event.stopPropagation();
+                    buttonAction();
+                  }}
+                > {buttonText} </button>
+              </div>
+            </div>
+          </Fragment>
+        );
+      };
+
+    const createNewPopUpFragment = () => {
+    return createPopupFragment('Add Task', newTodo, setNewTodo, 'Create Task', () => addTodo(newTodo), setNewPopupActive)
+    }
+      
+    const createEditPopUpFragment = () => {
+    return createPopupFragment('Edit Task', todoToEditText, (value) => setTodoToEditText(value), 'Update', () => editTodo(todoToEdit), setEditPopupActive)
+    }
 
     return (
       <div className="p-8">
@@ -106,37 +166,12 @@ const HomePage = () => {
             
             <div className="flex justify-between">
                 <h4 className="order-1 text-base text-[#61759b] uppercase font-normal mb-4">Your tasks</h4>
-{/*                 
-                <button id="dropdownRadioBgHoverButton" data-dropdown-toggle="dropdownRadioBgHover" className="order-2 transform -translate-y-6 -translate-x-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown radio <svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path></svg></button>
-
-                <div id="dropdownRadioBgHover" className="z-10 hidden w-48 bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600">
-                    <ul className="p-3 space-y-1 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownRadioBgHoverButton">
-                        <li>
-                            <div className="flex items-center p-2 rounded hover:bg-gray-100 dark:hover:bg-gray-600">
-                                <input id="default-radio-4" type="radio" value="Option 1" name="default-radio" className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"></input>
-                                <label for="default-radio-4" className="w-full ml-2 text-sm font-medium text-gray-900 rounded dark:text-gray-300">Default radio</label>
-                            </div>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-
-            <div className="flex justify-between">
-                <div className="relative w-full lg:max-w-sm">
-                    <select className="w-full p-2.5 text-gray-500 bg-white border rounded-md shadow-sm outline-none appearance-none focus:border-indigo-600">
-                        <option>ReactJS Dropdown</option>
-                        <option>Laravel 9 with React</option>
-                        <option>React with Tailwind CSS</option>
-                        <option>React With Headless UI</option>
-                    </select>
-                </div> */}
             </div>
             
             
             <div>
                 { todos.map(todo => ( 
-                    <div className={"relative bg-[#131A26] p-4 rounded-2xl flex items-center transition duration-500 cursor-pointer mb-4 hover:opacity-80"} 
-                    key={todo._id} onClick={() => completeTodo(todo._id)}>
+                    <div className="relative bg-[#131A26] p-4 rounded-2xl flex items-center transition duration-500 cursor-pointer mb-4 hover:opacity-80" key={todo._id} onClick={() => completeTodo(todo._id)}>
                         <div className={"w-5 h-5 mr-4 rounded-full bg-[#61759b] transition duration-400 " + (todo.complete ? " bg-[#D81E5B] bg-gradient-to-b from-[#D81E5B] to-[#8A4EFC]":"")}></div>
                         <div className={"text-base" + (todo.complete ? " line-through" : "")}>{todo.text}</div>
                             <TrashIcon className="fill-[#D81E5B] absolute top-1/2 right-4 transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center" 
@@ -147,7 +182,10 @@ const HomePage = () => {
                             }/>
                             <PencilSquareIcon className="absolute top-1/2 right-12 transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center" 
                                 onClick={(event) => {
-                                event.stopPropagation()
+                                    event.stopPropagation()
+                                    setEditPopupActive(true)
+                                    setTodoToEdit(todo)
+                                    setTodoToEditText(todo.text)
                                 }
                             }/>
 
@@ -160,32 +198,13 @@ const HomePage = () => {
             </div>
             
 
-            <div className="fixed bottom-8 right-8 flex items-center justify-center w-32 h-12 rounded-full text-base font-bold text-gray-200 bg-gradient-to-br from-[#D81E5B] to-[#8A4EFC] cursor-pointer" onClick={() => setPopupActive(true)}>
+            <div className="fixed bottom-8 right-8 flex items-center justify-center w-32 h-12 rounded-full text-base font-bold text-gray-200 bg-gradient-to-br from-[#D81E5B] to-[#8A4EFC] cursor-pointer" onClick={() => setNewPopupActive(true)}>
                 Add Task&nbsp;
                 <PlusCircleIcon className="h-6 w-6"/>
             </div>
-            {popupActive ? (
-                <Fragment> 
-                <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full max-w-[400px] bg-[#EEE] p-8 rounded-lg shadow-lg">    
-                    <XCircleIcon className="fill-[#D81E5B] absolute top-6 right-3 cursor-pointer transform -translate-y-1/2 w-6 h-6 rounded-full flex items-center justify-center" onClick={() => setPopupActive(false)}/>
-                    <div>
-                        <h3 className='text-gray-800 mb-4 font-bold'>Add Task</h3>
-                        <input 
-                            type="text" 
-                            className="translate-y-2 text-gray-800 appearance-none h-10 min-w-max border-0 focus:border-purple-600 outline-none bg-white p-4 rounded-2xl shadow-md w-full text-base" 
-                            onChange={(e) => {setNewTodo(e.target.value)}}
-                            value={newTodo}
-                        />
 
-                        <button className="translate-y-4 mt-4 h-9 w-40 translate-x-20 transform cursor-pointer rounded-3xl bg-gradient-to-r from-[#D81E5B] to-[#8A4EFC] text-center text-lg font-semibold" 
-                                onClick={ (event) =>  {
-                                            event.stopPropagation()
-                                            addTodo(newTodo)}
-                                        }>Create Task</button>
-                    </div>
-                </div>
-                </Fragment>
-            ) : ""}
+            {newPopupActive ? createNewPopUpFragment() : ""}
+            {editPopUpActive ? createEditPopUpFragment() : ""}
             
       </div>
     );
