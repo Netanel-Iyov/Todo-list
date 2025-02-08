e1beb5e9587be3d5f9a95392aaf399c8d8875f2e
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2025-02-08 21:15:36 +0200

    introduce dummy change

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -1,6 +1,8 @@
 export const API_BASE = process.env.REACT_APP_API_BASE
 // utility function to check if a user is already connected
 
+const b = 'b'
+
 export const checkIfConnected = () => {
     const token = localStorage.getItem('token')
     const first_name = localStorage.getItem('first_name')
