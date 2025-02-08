f3aaffb9ea7a31eead5e1a4640fc73b7afbd377f
Netanel-Iyov <81302786+Netanel-Iyov@users.noreply.github.com>
2025-02-08 20:35:33 +0200

    Update utils.js

--- client/src/utils/utils.js
+++ client/src/utils/utils.js
@@ -2,6 +2,7 @@ export const API_BASE = process.env.REACT_APP_API_BASE
 // utility function to check if a user is already connected
 
 export const checkIfConnected = () => {
+    const a = 'a'
     const token = localStorage.getItem('token')
     const first_name = localStorage.getItem('first_name')
 
